d4b754ada7be05b37c4d7f888bd88f27d98cfe30
bilcom87 <bilcom87@hotmail.com>
2021-05-22 16:05:44 +0300

    Ders 4 Ödev 5

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MyDictPro
+{
+  class MyDictionary<TKey,TValue>
+  {
+    TValue[] _valueArray;
+    TKey[] _keyArray;
+   
+    
+    public MyDictionary()
+    {
+      _keyArray = new TKey[0];
+      _valueArray = new TValue[0];
+      //dictList = new T1[0];
+    }
+
+    public int Length()
+    { return _keyArray.Length; }
+
+    public void Add(TKey tKey,TValue tValue)
+    {
+      TKey[] _tempKey = _keyArray;
+      _keyArray = new TKey[_keyArray.Length + 1];
+      for (int i = 0; i < _tempKey.Length; i++)
+      {
+        _keyArray[i] = _tempKey[i];
+      }
+      _keyArray[_keyArray.Length-1] = tKey;
+
+      TValue[] _tempValue = _valueArray;
+      _valueArray = new TValue[_valueArray.Length + 1];
+      for (int i = 0; i < _tempValue.Length; i++)
+      {
+        _valueArray[i] = _tempValue[i];
+      }
+      _valueArray[_valueArray.Length-1] = tValue;
+
+    }
+    public TValue this[TKey key]
+    {
+      get
+      {
+        return _valueArray[Array.IndexOf(_keyArray,key)];
+      }
+      set
+      {
+        _valueArray[Array.IndexOf(_keyArray, key)] = value;
+      }
+    }
+  }
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictPro
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Dictionary<int, string> orjDict = new Dictionary<int, string>();
+
+      MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+      orjDict.Add(14, "Eli Bay");
+      orjDict.Add(15, "Eli Bayd");
+
+      myDictionary.Add(1, "Apple");
+      myDictionary.Add(2, "Samsung");
+      myDictionary.Add(3, "LG");
+
+      Console.WriteLine("Dictionary listedeki veri : {0}", orjDict[14]);
+
+      Console.WriteLine("MyDictionary listedeki veri : {0}", myDictionary[1]);
+
+      Console.WriteLine("\n MyDictionary Listesi\n------------------------------\n");
+      for (int i = 1; i <= myDictionary.Length(); i++)
+      {
+        Console.WriteLine("{0} - {1}", i,myDictionary[i]);
+      }
+
+    }
+  }
+}
